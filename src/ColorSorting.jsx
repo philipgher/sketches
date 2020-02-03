0eb27ab30b3a6f5e179e1a20a317037b18383f3d
Philip Ghering <philip.ghering@random.studio>
2020-02-03 17:38:00 +0100

    Tweak algorithm

--- src/ColorSorting.jsx
+++ src/ColorSorting.jsx
@@ -10,111 +10,87 @@ import GeneticAlgorithm from './utils/GeneticAlgorithm';
 const swatches = originalSwatches.map((swatch, i) => ({ rgb: swatch, i }));
 console.log('swatches', swatches);
 
+const getFitnessForSwatch = (shapedPhenoType, swatch, i, j, row) => (
+  (i - 1 >= 0
+    ? Vibrant.Util.rgbDiff(swatch.rgb, shapedPhenoType[i - 1][j].rgb) : 0) // top
+    + (j + 1 < row.length
+      ? Vibrant.Util.rgbDiff(swatch.rgb, shapedPhenoType[i][j + 1].rgb) : 0) // right
+    + (i + 1 < shapedPhenoType.length && shapedPhenoType[i + 1][j] !== undefined
+      ? Vibrant.Util.rgbDiff(swatch.rgb, shapedPhenoType[i + 1][j].rgb) : 0) // bottom
+    + (j - 1 >= 0
+      ? Vibrant.Util.rgbDiff(swatch.rgb, shapedPhenoType[i][j - 1].rgb) : 0) // left
+    / 4 // to get average
+);
 
 // use oldPhenotype and some random function to make a change to your phenotype
 const mutationFunction = (oldPhenotype) => {
-  console.log('mutationFunc', oldPhenotype);
+  // const randomSamples = sampleSize(oldPhenotype.swatches, 10);
 
-  const randomSamples = sampleSize(oldPhenotype.swatches, 5);
+  const shapedPhenoType = reshape(oldPhenotype.swatches, 25);
 
-  const newPhenotype = { swatches: oldPhenotype.swatches };
+  const shapedPhenotypeDistances = shapedPhenoType.map((row, i) => (
+    row.map((swatch, j) => ({
+      score: getFitnessForSwatch(shapedPhenoType, swatch, i, j, row),
+      i: swatch.i,
+    }))
+  )).flat().sort((cur, prev) => cur.score - prev.score);
 
-  for (let i = 0; i < randomSamples.length; i += 1) {
-    newPhenotype.swatches.splice(
-      newPhenotype.swatches.indexOf(randomSamples[i]),
-      1,
-    );
+  console.log('shapedPhenotypeDistances', shapedPhenotypeDistances);
 
-    newPhenotype.swatches.splice(
-      Math.floor(Math.random() * oldPhenotype.swatches.length - 1),
-      0,
-      randomSamples[i],
-    );
-  }
+  const worstElements = shapedPhenotypeDistances.slice(oldPhenotype.swatches.length - 20, oldPhenotype.swatches.length);
+  console.log(worstElements);
 
-  console.log('mutationFunc', 'result', newPhenotype);
+
+  // const newPhenotype = { swatches: [...oldPhenotype.swatches] };
+  // for (let i = 0; i < randomSamples.length; i += 1) {
+  //   newPhenotype.swatches.splice(
+  //     newPhenotype.swatches.indexOf(randomSamples[i]),
+  //     1,
+  //   );
+  //   newPhenotype.swatches.splice(
+  //     Math.floor(Math.random() * oldPhenotype.swatches.length - 1),
+  //     0,
+  //     randomSamples[i],
+  //   );
+  // }
 
   return newPhenotype;
 };
 
-// use phenoTypeA and B to create phenotype result 1 and 2
-const crossoverFunction = (phenoTypeA, phenoTypeB) => {
-  console.log('crossoverfunc', phenoTypeA, phenoTypeB);
-
-  const matedPhenotypes = { swatches: uniqWith([...phenoTypeA.swatches, ...phenoTypeB.swatches], isEqual) };
-  console.log('crossoverfunc', 'result', matedPhenotypes);
-
-  return matedPhenotypes;
-};
-
 // use phenotype and possibly some other information to determine the fitness number.
-// Higher is better, lower is worse.
+// lower is better, higher is worse.
 const fitnessFunction = (phenotype) => {
-  console.log('fitnessfunc', phenotype);
-
-  if (!phenotype) {
-    return 0;
-  }
-
   const shapedPhenoType = reshape(phenotype.swatches, 25);
 
   const shapedPhenotypeDistances = shapedPhenoType.map((row, i) => (
-    row.map((swatch, j) => (
-      (i - 1 >= 0
-        ? Vibrant.Util.rgbDiff(swatch.rgb, shapedPhenoType[i - 1][j].rgb) : 0) // top
-      + (j + 1 < row.length
-        ? Vibrant.Util.rgbDiff(swatch.rgb, shapedPhenoType[i][j + 1].rgb) : 0) // right
-      + (i + 1 < shapedPhenoType.length && shapedPhenoType[i + 1][j] !== undefined
-        ? Vibrant.Util.rgbDiff(swatch.rgb, shapedPhenoType[i + 1][j].rgb) : 0) // bottom
-      + (j - 1 >= 0
-        ? Vibrant.Util.rgbDiff(swatch.rgb, shapedPhenoType[i][j - 1].rgb) : 0) // left
-      / 4 // to get average
-    ))
+    row.map((swatch, j) => getFitnessForSwatch(shapedPhenoType, swatch, i, j, row))
   ));
 
   const fitnessVal = (shapedPhenotypeDistances.flat().reduce(
     (prevVal, curVal) => prevVal + curVal, 0,
-  ) / swatches.length) * -1;
-
-  console.log('fitnessfunc', 'result', fitnessVal);
+  ) / swatches.length);
 
   return fitnessVal;
 };
 
-const geneConfig = {
+const evolution = new GeneticAlgorithm({
   mutationFunction,
-  crossoverFunction,
   fitnessFunction,
-  // doesABeatBFunction: yourCompetitionFunction,
-  population: [{ swatches }],
-  populationSize: 10, // defaults to 100
-};
-
-// const geneticalgorithm = GeneticAlgorithmConstructor(geneConfig);
-const geneticalgorithm = new GeneticAlgorithm(geneConfig);
+  firstIndividual: { swatches: [...swatches] },
+  populationSize: 50,
+});
 
 const ColorSorting = () => {
   const [swatchLayout, setSwatchLayout] = useState();
 
-  const evolution = useRef(geneticalgorithm);
-
-  // useInterval(() => {
-  //   console.log('evolution', evolution.current);
-
-  //   setSwatchLayout(reshape(evolution.current.best(), 25));
-  //   evolution.current = evolution.current.evolve();
-  // }, null);
+  useInterval(() => {
+    evolution.evolve();
+    setSwatchLayout(reshape(evolution.best().swatches, 25));
+  }, 50);
 
   const handleClickButton = () => {
-    console.log('evolution', evolution.current);
-    evolution.current = evolution.current.evolve();
-
-    const bestOfPopulation = evolution.current.best();
-    console.log('bestofpopulation', bestOfPopulation);
-
-    if (bestOfPopulation) {
-      setSwatchLayout(reshape(bestOfPopulation.swatches, 25));
-    }
+    evolution.evolve();
+    setSwatchLayout(reshape(evolution.best().swatches, 25));
   };
 
   return (
@@ -132,7 +108,7 @@ const ColorSorting = () => {
                 width: '35px',
                 height: '35px',
                 backgroundColor: `rgba(${swatch.rgb[0]}, ${swatch.rgb[1]}, ${swatch.rgb[2]})`,
-                transition: 'all 100ms ease-in-out',
+                // transition: 'all 100ms ease-in-out',
               }}
             />
           ))
--- src/utils/GeneticAlgorithm.js
+++ src/utils/GeneticAlgorithm.js
@@ -1,23 +1,48 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-undef */
+
 class GeneticAlgorithm {
   constructor(config) {
-    console.log(config);
+    // console.log(config);
+
     this.mutationFunction = config.mutationFunction;
     this.crossoverFunction = config.crossoverFunction;
     this.fitnessFunction = config.fitnessFunction;
-    this.population = config.population;
     this.populationSize = config.populationSize;
+
+    this.population = this._populate(config.firstIndividual, config.populationSize);
   }
 
-  _populate(firstSample, size) {
-    return [...Array(size)].map(() => this.mutateFunction(firstSample));
+  _populate(parent, size) {
+    return [...Array(size)].map(() => this.mutationFunction(parent));
+  }
+
+  _calculateFitnesses() {
+    const fitnesses = this.population.map((individual, i) => ({ score: this.fitnessFunction(individual), i }));
+
+    return fitnesses.sort((cur, prev) => cur.score - prev.score);
   }
 
   evolve() {
+    // first calculate all fitness scores for the current population
+    const fitnesses = this._calculateFitnesses();
 
+    // regenerate population from offspring of fittest individuals
+    const newPopulation = [...Array(10)].map((_, i) => this.population[fitnesses[i].i]);
+
+    let step = 0;
+    while (newPopulation.length < this.populationSize) {
+      newPopulation.push(this.mutationFunction(newPopulation[step % 20]));
+      step += 1;
+    }
+
+    this.population = newPopulation;
   }
 
   best() {
-
+    const fits = this._calculateFitnesses();
+    console.log('fittest value:', fits[0].score);
+    return this.population[fits[0].i];
   }
 }
 
